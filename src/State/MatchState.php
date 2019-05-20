5b42e8973df1d6980b6a56165fe2213a813f9404
Simon Eddy <simon@simoneddy.com.au>
2019-05-20 17:18:56 +1000

    initial commit

---
+++ src/State/MatchState.php
@@ -0,0 +1,13 @@
+<?php
+namespace Phooty\Simulation\State;
+
+/**
+ * The MatchState object should be a record of stats for a specific time in
+ * the match.
+ * 
+ * It should be immutable and return a new instance when changed.
+ */
+class MatchState
+{
+
+}
