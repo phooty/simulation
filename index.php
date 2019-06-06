c62c23ad1039e2a303f0fd1c95e66dda5b955dc8
Simon Eddy <simon@simoneddy.com.au>
2019-06-06 13:10:17 +1000

    playing with vardumper

--- index.php
+++ index.php
@@ -2,9 +2,17 @@
 use Phooty\Simulation\Kernel;
 use Carbon\Carbon;
 use Phooty\Simulation\Factory\TeamFactory;
+use Symfony\Component\VarDumper\Dumper\HtmlDumper;
+use Symfony\Component\VarDumper\Cloner\Data;
+
 //use Eddy\Tilemap\Visualizer\HtmlDumper;
 
 require __DIR__.'/vendor/autoload.php';
+
+//dump(memory_get_usage());
+$dumper = new HtmlDumper();
+
+$dumper->setTheme('light');
 
 $start = Carbon::now()->getTimestamp();
 
@@ -24,30 +32,21 @@ $sim = $kernel->makeSim(function ($builder) use ($home, $away) {
     $builder->setAwayTeam($away);
 });
 
-/* echo <<<EOT
-<html>
-<head>
-    <title>Phooty</title>
-    <style>
-        html {
-            background: black;
-            color: white;
-        }
-    </style>
-</head>
-<body>
-EOT; */
-
-dd($sim->run());
+$results = $sim->run();
 
 $total = Carbon::now()->getTimestamp() - $start;
 
 //HtmlDumper::dump($sim->getMatch()->getTilemap());
 //dd();
+$data = [];
 
-dump("Sim took {$total} microseconds");
-
+$data[] = "Sim took {$total} microseconds";
+//dump(memory_get_usage());
 //dd($kernel);
 //dump($sim->getMatch());
 
-//echo "</body></html>";
+$data = new Data($data);
+
+$dumper->dump($data);
+
+dump($results, $sim);
